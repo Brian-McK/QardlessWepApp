9bc743d3ee8c6468536da77744ae64649fb3daf0
Brian McKenna <D00197352@student.dkit.ie>
2023-02-07 16:56:31 +0000

    add overview base layout page

--- create-react-app/src/dashboard/Overview.js
+++ create-react-app/src/dashboard/Overview.js
@@ -5,51 +5,51 @@ import Paper from "@mui/material/Paper";
 
 export default function Overview() {
   return (
-    <Grid container spacing={3}>
-      {/* Overview */}
-      <Grid item xs={12} md={6} lg={6}>
-        <Paper
-          sx={{
-            p: 2,
-            display: "flex",
-            flexDirection: "column",
-            height: 240,
-          }}
-        ></Paper>
+      <Grid container spacing={3}>
+        {/* Overview */}
+        <Grid item xs={12} md={6} lg={6}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          ></Paper>
+        </Grid>
+        {/* Overview */}
+        <Grid item xs={12} md={6} lg={6}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          ></Paper>
+        </Grid>
+        {/* Overview */}
+        <Grid item xs={12} md={6} lg={6}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          ></Paper>
+        </Grid>
+        {/* Overview */}
+        <Grid item xs={12} md={6} lg={6}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          ></Paper>
+        </Grid>
       </Grid>
-      {/* Overview */}
-      <Grid item xs={12} md={6} lg={6}>
-        <Paper
-          sx={{
-            p: 2,
-            display: "flex",
-            flexDirection: "column",
-            height: 240,
-          }}
-        ></Paper>
-      </Grid>
-      {/* Overview */}
-      <Grid item xs={12} md={6} lg={6}>
-        <Paper
-          sx={{
-            p: 2,
-            display: "flex",
-            flexDirection: "column",
-            height: 240,
-          }}
-        ></Paper>
-      </Grid>
-      {/* Overview */}
-      <Grid item xs={12} md={6} lg={6}>
-        <Paper
-          sx={{
-            p: 2,
-            display: "flex",
-            flexDirection: "column",
-            height: 240,
-          }}
-        ></Paper>
-      </Grid>
-    </Grid>
   );
 }
