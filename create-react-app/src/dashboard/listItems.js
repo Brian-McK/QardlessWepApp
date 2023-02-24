7c567918224290504209d5283c3a0d1d00bb4763
Brian McKenna <D00197352@student.dkit.ie>
2023-02-24 18:28:25 +0000

    adjusted listitems

--- create-react-app/src/dashboard/listItems.js
+++ create-react-app/src/dashboard/listItems.js
@@ -6,29 +6,53 @@ import ListItemText from "@mui/material/ListItemText";
 import HomeIcon from "@mui/icons-material/Home";
 import NoteAddIcon from "@mui/icons-material/NoteAdd";
 import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
+import ListIcon from '@mui/icons-material/List';
 
 export const mainListItems = (
   <React.Fragment>
+    {/* Overview */}
     <ListItemButton>
       <ListItemIcon>
         <HomeIcon />
       </ListItemIcon>
       <ListItemText primary="Overview" />
     </ListItemButton>
+    {/* Overview */}
+
+    {/* Courses */}
+    <ListSubheader component="div" inset>
+      Manage Courses
+    </ListSubheader>
+    <ListItemButton>
+      <ListItemIcon>
+        <ListIcon />
+      </ListItemIcon>
+      <ListItemText primary="View Courses" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <NoteAddIcon />
+      </ListItemIcon>
+      <ListItemText primary="Add Course" />
+    </ListItemButton>
+    {/* Courses */}
+
+    {/* Certificates */}
     <ListSubheader component="div" inset>
       Manage Certificates
     </ListSubheader>
     <ListItemButton>
       <ListItemIcon>
-        <NoteAddIcon />
+        <ListIcon />
       </ListItemIcon>
-      <ListItemText primary="Create Certificates" />
+      <ListItemText primary="View Certificates" />
     </ListItemButton>
     <ListItemButton>
       <ListItemIcon>
         <AssignmentTurnedInIcon />
       </ListItemIcon>
-      <ListItemText primary="Assign Certificates" />
+      <ListItemText primary="Add Certificate" />
     </ListItemButton>
+    {/* Certificates */}
   </React.Fragment>
 );
