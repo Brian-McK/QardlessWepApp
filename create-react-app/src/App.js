1099405f90d84db53b77285077c184be8ef3a5bc
Brian McKenna <D00197352@student.dkit.ie>
2023-02-26 19:22:17 +0000

    login component created

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,12 +1,14 @@
 import * as React from "react";
 import Dashboard from "../src/dashboard/Dashboard";
 import { SharedSnackbarProvider } from "./providers/SharedSnackbar.context";
+import Login from "./dashboard/Login/Login";
 
 export default function App() {
   return (
     <div>
       <SharedSnackbarProvider>
-        <Dashboard />
+        {/* <Dashboard /> */}
+        <Login />
       </SharedSnackbarProvider>
     </div>
   );
