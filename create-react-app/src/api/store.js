27fb34113e1d3e4d9302f0898a0d8d610e4faa1c
Brian McKenna <D00197352@student.dkit.ie>
2023-02-26 13:32:40 +0000

    add courses api redux store configs

--- create-react-app/src/api/store.js
+++ create-react-app/src/api/store.js
@@ -2,18 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { certificatesApi } from "./services/certificates";
 import { endUsersApi } from "./services/endusers";
+import { coursesApi } from "./services/courses";
 
 export const store = configureStore({
   reducer: {
     [endUsersApi.reducerPath]: endUsersApi.reducer,
     [certificatesApi.reducerPath]: certificatesApi.reducer,
+    [coursesApi.reducerPath]: coursesApi.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(endUsersApi.middleware)
-      .concat(certificatesApi.middleware),
+      .concat(certificatesApi.middleware)
+      .concat(coursesApi.middleware),
 });
 
 setupListeners(store.dispatch);
