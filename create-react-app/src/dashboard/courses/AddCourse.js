0bd1124c787b10b38a270b068bcabdee2550050f
Brian McKenna <D00197352@student.dkit.ie>
2023-03-06 21:59:51 +0000

    remove unwanted code and imports

--- create-react-app/src/dashboard/courses/AddCourse.js
+++ create-react-app/src/dashboard/courses/AddCourse.js
@@ -8,10 +8,7 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs from "dayjs";
-import {
-  SharedSnackbarConsumer,
-  SharedSnackbarContext,
-} from "../../providers/SharedSnackbar.context";
+import { SharedSnackbarContext } from "../../providers/SharedSnackbar.context";
 import { useAddCourseMutation } from "../../api/services/courses";
 
 const today = dayjs();
@@ -26,7 +23,7 @@ const validationSchemaAddCourse = yup.object({
 });
 
 export default function AddCourse() {
-  const [addCourse, { data, isError, isLoading, isSuccess, error }] =
+  const [addCourse, { data, isError, isLoading, isSuccess }] =
     useAddCourseMutation();
 
   const snackBarContext = React.useContext(SharedSnackbarContext);
@@ -69,118 +66,106 @@ export default function AddCourse() {
     },
   });
   return (
-    <SharedSnackbarConsumer>
-      {({ openSnackbar }) => (
-        <Grid container justifyContent={"center"} spacing={2}>
-          <Grid item xs={10} sm={8} md={6}>
-            <Paper
-              sx={{
-                p: 2,
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              <h4>Add Course</h4>
-              {/* Form Start */}
+    <Grid container justifyContent={"center"} spacing={2}>
+      <Grid item xs={10} sm={8} md={6}>
+        <Paper
+          sx={{
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <h4>Add Course</h4>
+          {/* Form Start */}
 
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <form onSubmit={formikAddCourse.handleSubmit}>
-                  <Stack spacing={2}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <form onSubmit={formikAddCourse.handleSubmit}>
+              <Stack spacing={2}>
+                <TextField
+                  variant="filled"
+                  fullWidth
+                  id="courseTitle"
+                  type="text"
+                  name="courseTitle"
+                  label="Course Title"
+                  value={formikAddCourse.values.courseTitle}
+                  onChange={formikAddCourse.handleChange}
+                  error={
+                    formikAddCourse.touched.courseTitle &&
+                    Boolean(formikAddCourse.errors.courseTitle)
+                  }
+                  helperText={
+                    formikAddCourse.touched.courseTitle &&
+                    formikAddCourse.errors.courseTitle
+                  }
+                />
+
+                <DatePicker
+                  inputFormat="DD/MM/YYYY"
+                  label="Course Start Date"
+                  views={["year", "month", "day"]}
+                  value={formikAddCourse.values.startDate}
+                  onChange={(value) =>
+                    formikAddCourse.setFieldValue("startDate", value)
+                  }
+                  renderInput={(params) => (
                     <TextField
                       variant="filled"
                       fullWidth
-                      id="courseTitle"
-                      type="text"
-                      name="courseTitle"
-                      label="Course Title"
-                      value={formikAddCourse.values.courseTitle}
-                      onChange={formikAddCourse.handleChange}
+                      {...params}
                       error={
-                        formikAddCourse.touched.courseTitle &&
-                        Boolean(formikAddCourse.errors.courseTitle)
+                        formikAddCourse.touched.startDate &&
+                        Boolean(formikAddCourse.errors.startDate)
                       }
                       helperText={
-                        formikAddCourse.touched.courseTitle &&
-                        formikAddCourse.errors.courseTitle
+                        formikAddCourse.touched.startDate &&
+                        formikAddCourse.errors.startDate
                       }
                     />
+                  )}
+                />
 
-                    <DatePicker
-                      inputFormat="DD/MM/YYYY"
-                      label="Course Start Date"
-                      views={["year", "month", "day"]}
-                      value={formikAddCourse.values.startDate}
-                      onChange={(value) =>
-                        formikAddCourse.setFieldValue("startDate", value)
-                      }
-                      renderInput={(params) => (
-                        <TextField
-                          variant="filled"
-                          fullWidth
-                          {...params}
-                          error={
-                            formikAddCourse.touched.startDate &&
-                            Boolean(formikAddCourse.errors.startDate)
-                          }
-                          helperText={
-                            formikAddCourse.touched.startDate &&
-                            formikAddCourse.errors.startDate
-                          }
-                        />
-                      )}
-                    />
-
-                    <DatePicker
-                      inputFormat="DD/MM/YYYY"
-                      disablePast
-                      label="Course Expiry Date"
-                      views={["year", "month", "day"]}
-                      value={formikAddCourse.values.expiryDate}
-                      onChange={(value) =>
-                        formikAddCourse.setFieldValue("expiryDate", value)
-                      }
-                      renderInput={(params) => (
-                        <TextField
-                          variant="filled"
-                          fullWidth
-                          {...params}
-                          error={
-                            formikAddCourse.touched.expiryDate &&
-                            Boolean(formikAddCourse.errors.expiryDate)
-                          }
-                          helperText={
-                            formikAddCourse.touched.expiryDate &&
-                            formikAddCourse.errors.expiryDate
-                          }
-                        />
-                      )}
-                    />
-
-                    <Button
-                      color="primary"
-                      variant="contained"
+                <DatePicker
+                  inputFormat="DD/MM/YYYY"
+                  disablePast
+                  label="Course Expiry Date"
+                  views={["year", "month", "day"]}
+                  value={formikAddCourse.values.expiryDate}
+                  onChange={(value) =>
+                    formikAddCourse.setFieldValue("expiryDate", value)
+                  }
+                  renderInput={(params) => (
+                    <TextField
+                      variant="filled"
                       fullWidth
-                      disabled={isLoading}
-                      type="submit"
-                    >
-                      Submit
-                    </Button>
+                      {...params}
+                      error={
+                        formikAddCourse.touched.expiryDate &&
+                        Boolean(formikAddCourse.errors.expiryDate)
+                      }
+                      helperText={
+                        formikAddCourse.touched.expiryDate &&
+                        formikAddCourse.errors.expiryDate
+                      }
+                    />
+                  )}
+                />
 
-                    <p>{"isLoading: " + isLoading}</p>
-
-                    <p>{"isSuccess: " + isSuccess}</p>
-
-                    <p>{"isError: " + isError}</p>
-
-                    <p>{"data: " + JSON.stringify(data)}</p>
-                  </Stack>
-                </form>
-              </LocalizationProvider>
-              {/* Form End */}
-            </Paper>
-          </Grid>
-        </Grid>
-      )}
-    </SharedSnackbarConsumer>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  fullWidth
+                  disabled={isLoading}
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </Stack>
+            </form>
+          </LocalizationProvider>
+          {/* Form End */}
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
