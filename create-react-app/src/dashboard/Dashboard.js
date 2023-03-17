6b2455841c8463096ecafb713b6a29f557ec4272
Brian McKenna <D00197352@student.dkit.ie>
2023-03-17 18:41:14 +0000

    change side nav width

--- create-react-app/src/dashboard/Dashboard.js
+++ create-react-app/src/dashboard/Dashboard.js
@@ -18,7 +18,7 @@ import ListItems from "./listItems";
 import { useAuth } from "../providers/Auth.context";
 import MainContent from "./MainContent";
 
-const drawerWidth = 240;
+const drawerWidth = 260;
 
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== "open",
