700cbe65773658b349ebac63553cbfb4ffe549d3
Brian McKenna <D00197352@student.dkit.ie>
2023-04-27 19:43:24 +0100

    add number of active certs

---
+++ create-react-app/src/dashboard/Overview/ActiveCerts.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import Title from "../Title";
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+export default function ActiveCerts({ certData }) {
+  const activeCerts = certData.filter((c) => c.isFrozen === false).length;
+
+  console.log(activeCerts);
+
+  return (
+    <>
+      <Title>Total Active Certs</Title>
+      <Typography component="p" variant="h4">
+        {certData.length}
+      </Typography>
+      <Typography color="text.secondary" sx={{ flex: 1 }}>
+        on 15 March, 2019
+      </Typography>
+      <div>
+        <Link color="primary" href="certificates">
+          View certificates
+        </Link>
+      </div>
+    </>
+  );
+}
--- create-react-app/src/dashboard/Overview/Overview.js
+++ create-react-app/src/dashboard/Overview/Overview.js
@@ -2,13 +2,28 @@ import * as React from "react";
 import Title from "../Title";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import ActiveCerts from "./ActiveCerts";
+import { useGetAllCertificatesByBusinessIdQuery } from "../../api/services/certificates";
+import { useAuth } from "../../providers/Auth.context";
 
 export default function Overview() {
+  const { user } = useAuth();
+
+  const {
+    data: allCerts = [],
+    error: allCertsError,
+    isLoading: allCertsIsLoading,
+    isError: allCertsIsError,
+    isSuccess: allCertsIsSuccess,
+  } = useGetAllCertificatesByBusinessIdQuery(user.businessId);
+
+  console.log(allCerts);
+
   return (
     <>
       <Title>Overview</Title>
       <Grid container spacing={3}>
-        <Grid item xs={12} md={4} >
+        <Grid item xs={12} md={4}>
           <Paper
             sx={{
               p: 2,
@@ -17,7 +32,7 @@ export default function Overview() {
               height: 240,
             }}
           >
-            
+            <ActiveCerts certData={allCerts} />
           </Paper>
         </Grid>
         <Grid item xs={12} md={4}>
